4f5e1ad806a198f06fea57542bda0ebcaa2993cc
msayagh <sayaghmohammed@gmail.com>
2025-03-13 23:31:55 +0400

    docker

--- pipeline/script.py
+++ pipeline/script.py
@@ -3,15 +3,15 @@ import time
 
 # Configure logging
 logging.basicConfig(
-    filename="logs/app.log",
+    filename="/logs/app.log",
     level=logging.INFO,
     format="%(asctime)s - %(levelname)s - %(message)s",
 )
 
 # Generate logs
 if __name__ == "__main__":
-    for i in range(10):
-        logging.info(f"R2- Hello World Log {i}")
+    for i in range(1000):
+        logging.info(f"R120- Hello World Log {i}")
         time.sleep(1)
 
-    logging.error("R2- This is an error message")
+    logging.error("R120- This is an error message")
